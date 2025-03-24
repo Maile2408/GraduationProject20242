4cad1920454f659037d94deec1c11a23fbf2b278
tranmaile2002 <tranmaile2002@gmail.com>
2025-03-25 00:59:07 +0700

    Create Building Data, Building Model Prefabs

---
+++ .history/Assets/Project/Scripts/Gameplay/Ingame/Worker/BuildingWorkers_20250324212402.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingWorkers : Workers
+{
+    [SerializeField] protected int maxWorker = 1;
+    [SerializeField] protected List<WorkerCtrl> workers;
+    //[SerializeField] protected List<Transform> workers;
+
+    public virtual bool IsNeedWorker()
+    {
+        if (this.workers.Count >= this.maxWorker) return false;
+        return true;
+    }
+
+    public virtual void AddWorker(WorkerCtrl worker)
+    {
+        this.workers.Add(worker);
+    }
+
+    /*public virtual void ReleaseWorkers()
+    {
+        foreach(WorkerCtrl workerCtrl in this.workers)
+        {
+            workerCtrl.WorkerReleased();
+        }
+
+        this.workers.Clear();
+    }*/
+}
